be621746871408221d674384e416500854318f1b
Menel Robot <futszak@menel.tkruk.it>
2019-03-22 20:52:40 +0100

    fix sndmail function

--- sndmail.py
+++ sndmail.py
@@ -1,24 +1,16 @@
-# follow.py
+# sndmail.py
 #
-# Follow a file like tail -f.
+# Sending mails
 
 import smtplib
 from email.mime.text import MIMEText
 
-config = {
-    'SRCMAIL': "security@menel.tkruk.it",
-    'HOSTNAME': "localhost"
-    }
 
-
-def sndmail(subject, dstmail, content="no content"):
+def sndmail(hostname,sender,reciever,subject,content="no content"):
         msg = MIMEText(content)
         msg['Subject'] = subject
-        msg['From'] = config['SRCMAIL']
-        msg['To'] = dstmail
-        s = smtplib.SMTP(config['HOSTNAME'])
-        s.sendmail(config['SRCMAIL'], dstmail, msg.as_string())
+        msg['From'] = sender
+        msg['To'] = reciever
+        s = smtplib.SMTP(hostname)
+        s.sendmail(sender, reciever, msg.as_string())
         s.quit()
-
-def sndmail1(subject, dstmail, content="no content"):
-	print(subject)
